f654a1e2612eea188b1d6022564fe591c08c8b33
ema <lotofacundo6@gmail.com>
2024-02-11 18:31:29 -0300

    config git ignore

---
+++ target/generated-sources/annotations/com/demo/contacts/User/Persistence/Mapper/UserMapperImpl.java
@@ -0,0 +1,55 @@
+package com.demo.contacts.User.Persistence.Mapper;
+
+import com.demo.contacts.User.Domain.Dto.UserDto;
+import com.demo.contacts.User.Persistence.Entity.UserEntity;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-11T18:22:16-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto mapToDto(UserEntity user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        String lastName = null;
+        long id = 0L;
+        String name = null;
+        String email = null;
+        String password = null;
+
+        lastName = user.getLastName();
+        if ( user.getId() != null ) {
+            id = user.getId();
+        }
+        name = user.getName();
+        email = user.getEmail();
+        password = user.getPassword();
+
+        UserDto userDto = new UserDto( id, name, lastName, email, password );
+
+        return userDto;
+    }
+
+    @Override
+    public UserEntity mapToEntity(UserDto userDTO) {
+        if ( userDTO == null ) {
+            return null;
+        }
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setLastName( userDTO.getLastName() );
+        userEntity.setId( userDTO.getId() );
+        userEntity.setName( userDTO.getName() );
+        userEntity.setEmail( userDTO.getEmail() );
+        userEntity.setPassword( userDTO.getPassword() );
+
+        return userEntity;
+    }
+}
