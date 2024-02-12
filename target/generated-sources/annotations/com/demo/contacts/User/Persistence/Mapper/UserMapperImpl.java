b337905668d4b3b4f56d2ed95133796e79ccf79a
ema <lotofacundo6@gmail.com>
2024-02-12 16:41:48 -0300

    fix mysql url

--- target/generated-sources/annotations/com/demo/contacts/User/Persistence/Mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/demo/contacts/User/Persistence/Mapper/UserMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-11T18:22:16-0300",
+    date = "2024-02-12T16:41:13-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
