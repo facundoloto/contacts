f4b5007f56678e882125e6badd00492e1016506d
ema <lotofacundo6@gmail.com>
2024-02-23 16:27:56 -0300

    fix cors config

--- target/generated-sources/annotations/com/demo/contacts/User/Persistence/Mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/demo/contacts/User/Persistence/Mapper/UserMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-20T13:38:32-0300",
+    date = "2024-02-23T16:27:25-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
